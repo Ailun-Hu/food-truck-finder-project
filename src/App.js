32bf66cd705cebbae44290e3dd1d4bfeada74163
Alan Hu <64699846+Ailun-Hu@users.noreply.github.com>
2024-02-20 21:59:58 -0500

    added map to app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+  InfoWindow,
+} from "@vis.gl/react-google-maps";
 
 function App() {
+  const position = { lat: 40.7128, lng: -74.0060 };
+  const TestMarker = { lat: 40.7678, lng: -73.9645 };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <APIProvider apiKey="AIzaSyDpi9UQH0ORtprLFkNfrjFsY_kGYFzyGGQ">
+        <div style={{height: "100vh", width: "100%"}}>
+          <Map zoom={11} center={position} mapId="3a1d06301097d67f">
+            <AdvancedMarker position={TestMarker}>
+              {/*<Pin background={"grey"}></Pin>*/}
+            </AdvancedMarker>
+          </Map>
+        </div>
+      </APIProvider>
+    </main>
   );
 }
 
