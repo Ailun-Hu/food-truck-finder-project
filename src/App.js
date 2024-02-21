810d3f581bc3c911444a081198449911b281dcfc
Alan Hu <64699846+Ailun-Hu@users.noreply.github.com>
2024-02-20 22:29:28 -0500

    changed app.js code

--- src/App.js
+++ src/App.js
@@ -10,15 +10,18 @@ import {
 function App() {
   const position = { lat: 40.7128, lng: -74.0060 };
   const TestMarker = { lat: 40.7678, lng: -73.9645 };
+  const [open, setOpen] = useState(false);
+
 
   return (
     <main>
       <APIProvider apiKey="AIzaSyDpi9UQH0ORtprLFkNfrjFsY_kGYFzyGGQ">
         <div style={{height: "100vh", width: "100%"}}>
-          <Map zoom={11} center={position} mapId="3a1d06301097d67f">
-            <AdvancedMarker position={TestMarker}>
+          <Map streetViewControl={false} defaultCenter={TestMarker} zoom={15} mapId="3a1d06301097d67f">
+            <AdvancedMarker position={TestMarker} onClick={() => setOpen(true)}>
               {/*<Pin background={"grey"}></Pin>*/}
             </AdvancedMarker>
+            {open && <InfoWindow position={TestMarker}>Hunter College</InfoWindow>}
           </Map>
         </div>
       </APIProvider>
